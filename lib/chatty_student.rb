78ab67a6ae7e281b9de934c9d0059000d7c43b5a
William Mittard <billymittard@ymail.com>
2020-04-29 19:34:08 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,6 +7,6 @@ class ChattyStudent < Student
   
 def raise_hand 
   super
-  puts "Pick me!","Pick me!",
+  puts "Pick me!","Pick me!","Pick me!","Pick me!","Pick me!","Pick me!","Pick me!","Pick me!","Pick me!","Pick me!"
 end 
 end 
