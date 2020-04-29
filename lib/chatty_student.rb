18af2886603ae6a17079eb59d1c02a0ab944cb5e
William Mittard <billymittard@ymail.com>
2020-04-29 19:29:02 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,9 @@
-class ChattyStudent 
+class ChattyStudent < Student 
   def hello 
-    puts ""
+    puts "Hey there! I'm so excited to learn stuff."
+  end 
+  
+def raise_hand 
+  puts "Pick me!"
+end 
 end 
